bc6fd26d7f2f66e5437634a6bcfded458148e299
denilsonbarahona <91sbarahona@gmail.com>
2022-05-24 20:09:41 -0600

    [update] add layout mediaquery

--- src/infraestructure/ui/styles/containers/message-display.style.js
+++ src/infraestructure/ui/styles/containers/message-display.style.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  display: grid;
   grid-template-rows: 5rem 1fr 4rem;
   block-size: 100vh;
+  display: none;
+
+  @media screen and (min-width: 1024px) {
+    & {
+      display: grid;
+    }
+  }
 `;
 
 export const MessageContainer = styled.div`
--- src/infraestructure/ui/styles/layouts/app-layout.style.js
+++ src/infraestructure/ui/styles/layouts/app-layout.style.js
@@ -2,8 +2,14 @@ import styled from "styled-components";
 
 const Layout = styled.div`
   display: grid;
-  grid-template-columns: minmax(330px, 439px) minmax(694px, 1fr);
+  grid-template-columns: 1fr;
   block-size: 100vh;
+
+  @media screen and (min-width: 1024px) {
+    & {
+      grid-template-columns: minmax(330px, 439px) minmax(694px, 1fr);
+    }
+  }
 `;
 
 export default Layout;
