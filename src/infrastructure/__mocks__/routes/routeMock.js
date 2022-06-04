6abb14ada6d6a67ba3668df7d3b077cbb392db76
denilsonbarahona <91sbarahona@gmail.com>
2022-06-03 22:56:59 -0600

    [adding] adding mocks in the project

--- src/infrastructure/__mocks__/routes/routeMock.js
+++ src/infrastructure/__mocks__/routes/routeMock.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Router } from "react-router-dom";
+import PropTypes from "prop-types";
 import { createMemoryHistory } from "history";
 
 const RouterMock = ({ children }) => {
@@ -9,6 +10,14 @@ const RouterMock = ({ children }) => {
       {children}
     </Router>
   );
+};
+
+RouterMock.propTypes = {
+  children: PropTypes.node,
+};
+
+RouterMock.defaultProps = {
+  children: null,
 };
 
 export default RouterMock;
