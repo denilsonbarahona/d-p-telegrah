c1adfee479e326b9b0dea296b67e81dafe2bb16c
denilsonbarahona <91sbarahona@gmail.com>
2022-06-03 22:54:28 -0600

    [update] updating style in ui

--- src/infrastructure/ui/styles/layouts/side-layout.style.js
+++ src/infrastructure/ui/styles/layouts/side-layout.style.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const Container = styled.div`
+const Container = styled.aside`
   display: grid;
   grid-template-rows: 5rem 1fr 4rem;
   block-size: 100vh;
