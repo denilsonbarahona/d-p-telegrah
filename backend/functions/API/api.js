0743b0c9661c4c7fd83cd8e90929e75c0ed4f4e1
denilsonbarahona <91sbarahona@gmail.com>
2022-06-10 23:03:57 -0600

    [update] with linter format

--- backend/functions/API/api.js
+++ backend/functions/API/api.js
@@ -5,8 +5,8 @@ const db = require("../entities/firestore");
 const app = express();
 app.use(cors());
 
-app.get("/api/users", usersController.getUsers({ db }));
-app.get("/api/users/:id", usersController.getUserById({ db }));
-app.post("/api/users", usersController.createUser({ db }));
+app.get("/api/users", usersController.getUsers({db}));
+app.get("/api/users/:id", usersController.getUserById({db}));
+app.post("/api/users", usersController.createUser({db}));
 
 module.exports = app;
